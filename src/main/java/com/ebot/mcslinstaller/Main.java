6a8415a21c460855a200010a0ecfec604196a186
Exos Nguyen <esoft288@gmail.com>
2020-02-29 14:03:18 +0700

    29/02/20 - Add comments to code

--- src/main/java/com/ebot/mcslinstaller/Main.java
+++ src/main/java/com/ebot/mcslinstaller/Main.java
@@ -2,6 +2,9 @@ package com.ebot.mcslinstaller;
 
 import java.util.Arrays;
 
+/**
+ * Start point
+ */
 public class Main {
     public static final String path = System.getenv("APPDATA") + "\\.minecraft";
     public static final String javaPath = Arrays.stream(System.getenv("path").split(";")).filter(p->p.contains("Java")).toArray()[0].toString();
